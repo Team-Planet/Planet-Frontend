169dcf1326be5a863f0734836fd3b3f1e3065ade
nrllahh <atasnurullahh@gmail.com>
2024-05-13 23:08:18 +0300

    React router dom created

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import './App.css'
-
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import SignInPage from "./pages/SignInPage";
 
 function App() {
-
   return (
     <>
-      {/* Nurullah tarafından route geliştirmesi */}
+      <Routes>
+        {/* <Route path="/" element={<HomePage />} /> */}
+        <Route path="/SignIn" element={<SignInPage />} />
+        {/* <Route path="/SignUp" element={<SignUpPage />} />
+        <Route path="*" element={<NotFoundPage />} /> */}
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
