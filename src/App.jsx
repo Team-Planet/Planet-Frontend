7b82d4b8d7febae3a6cbf27c8e3b4ba51614792c
Emre Özgenç <emreozgenc@hotmail.com.tr>
2024-05-18 18:21:59 +0300

    feat : added roboto font address to the index.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,3 @@
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
 import "./App.css";
 import { Routes, Route, Navigate } from "react-router-dom";
 import SignInPage from "./pages/SignInPage";
